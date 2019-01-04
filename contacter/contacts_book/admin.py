27206673ce00bc93f577dd2e27493c083c0ebb06
Gordi91 <szymon.pecuch@hotmail.com>
2019-01-04 21:04:26 +0100

    Set up Django administration panel

--- contacter/contacts_book/admin.py
+++ contacter/contacts_book/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from contacts_book import models
 
-# Register your models here.
+
+@admin.register(models.Person)
+class PersonAdmin(admin.ModelAdmin):
+    filter_horizontal = ('groups',)
+
+
+@admin.register(models.Group)
+class GroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('members',)
+
+
+@admin.register(models.Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Email)
+class EmailAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    pass
